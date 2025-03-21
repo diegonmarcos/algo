a182d913f8731cb9a7d79d168d22a238682018b6
Diego <diegonmarcos@gmail.com>
2025-03-21 19:13:53 +0100

    docker

--- C2_push_swap/2_CODE/log/8.Docker/login.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh && chmod 777 install.sh && ./install.sh && #git clone --depth=1 https://github.com/romkatv/powerlevel10k.git \"/home/dinepomu/.oh-my-zsh/custom/themes/powerlevel10k\" && #sed -i 's/ZSH_THEME=\"robbyrussell\"/ZSH_THEME=\"powerlevel10k\/powerlevel10k\"/' ~/.zshrc
-chsh -s /usr/bin/zsh
