45b6710451b340cd7d8f7185edce5597b96389cb
Diego <diegonmarcos@gmail.com>
2025-03-26 19:01:33 +0100

    up

--- C2_push_swap/2_CODE/src/push_swap/tracers/1_2_debugger_helpers.c
+++ C2_push_swap/2_CODE/src/push_swap/tracers/1_2_debugger_helpers.c
@@ -6,7 +6,7 @@
 /*   By: dinepomu <dinepomu@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/25 09:52:30 by dinepomu          #+#    #+#             */
-/*   Updated: 2025/03/10 09:38:21 by dinepomu         ###   ########.fr       */
+/*   Updated: 2025/03/26 18:54:47 by dinepomu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@
 
 #include "push_swap.h"
 
+//quick_sort_pushswap(list_program);
 void	sort_stack_dbg(t_list_program *list_program)
 {
 	if (!ft_strcmp(ALGO, "selec"))
@@ -23,13 +24,13 @@ void	sort_stack_dbg(t_list_program *list_program)
 	else if (!ft_strcmp(ALGO, "radix"))
 		radix_sort_pushswap(list_program);
 	else if (!ft_strcmp(ALGO, "quick"))
-		quick_sort_pushswap(list_program);
+		gear_sort_pushswap(list_program);
 	else if (!ft_strcmp(ALGO, "gear"))
 		gear_sort_pushswap(list_program);
 	if (list_program->stack_a_size == 501)
 		radix_sort_pushswap(list_program);
 	else if (list_program->stack_a_size == 502)
-		quick_sort_pushswap(list_program);
+		gear_sort_pushswap(list_program);
 	else if (list_program->stack_a_size == 503)
 		gear_sort_pushswap(list_program);
 }
